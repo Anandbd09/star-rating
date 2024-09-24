1ec60409ac828dbb9282a54d798a5b869a3d5e74
Anandbd09 <anandbdundappanavar@gmail.com>
2024-09-24 01:09:02 -0700

    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Index from "./components/Index";
+
+function App() {
+  return (
+    <>
+      <Index noOfStars={10} />
+    </>
+  );
+}
+
+export default App;
